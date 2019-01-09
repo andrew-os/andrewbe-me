17c01b66478b853649eef56fac596fb15149ac7b
Andrew Sleigh <andrewsleigh@akauk.com>
2019-01-09 07:40:48 +0000

    markup and styles for footer, links not yet coded

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import './Footer.scss'
 
+let socialItems=['linkedin', 'github', 'spotify']
 export const Footer: React.StatelessComponent<{}> = () => {
     return (
         <div className="footer">
                 <p>&copy; andrewbeme </p>
+                <ul className="social-nav">
+                    {
+                        socialItems.map( (social) => {
+                            return <li>{social}</li>
+                        })
+                    }
+                </ul>
         </div>
     )
 }
